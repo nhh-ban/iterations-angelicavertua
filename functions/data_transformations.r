96d34e9dc2717612c78e51c4215e7bd2fc79ad97
angelicavertua <146018290+angelicavertua@users.noreply.github.com>
2023-10-11 17:20:00 +0200

    adding to_iso8601 function to the file data_transformations.r

--- functions/data_transformations.r
+++ functions/data_transformations.r
@@ -11,3 +11,11 @@ transform_metadata_to_df <- function(stations_metadata) {
     unnest_wider(location) |> 
     unnest_wider(latLon)
 }
+
+
+to_iso8601 <- function(date_time_var, offset_in_days) {
+  modified_date <- lubridate::as_datetime(date_time_var) + lubridate::days(offset_in_days)
+  iso8601_date <- paste(format(modified_date, tz="UTC"), "Z", sep = "")
+  return(iso8601_date)
+}
+
