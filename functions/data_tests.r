bb6a75386883a274306c355c7b490b8f76273684
angelicavertua <146018290+angelicavertua@users.noreply.github.com>
2023-10-11 16:42:20 +0200

    adding comments to test functions

--- functions/data_tests.r
+++ functions/data_tests.r
@@ -5,6 +5,12 @@
 # All tests are packed in a function test_stations_metadata that apples
 # all the aforementioned tests
 
+#this function it's designed to check whether a given data frame df 
+#has the expected column names ("id", "name", "latestData", "lat", "lon") 
+#if the columns do not match, the function provides a "FAIL" message, signaling 
+#that the data may not be in the expected format. If they do match, it provides
+#a "PASS" message, indicating that the correct column names have been specified
+
 test_stations_metadata_colnames <-
   function(df) {
     
@@ -16,6 +22,12 @@ test_stations_metadata_colnames <-
       print("FAIL: Columns do not match the correct specification")
     }
   }
+
+
+#this function it's designed to check the number of rows in a given data frame df 
+#and determine whether it falls within a specified range of expected row counts
+#It defines min_expected_rows and max_expected_rows as the minimum and maximum 
+#expected row counts and then compares the number of rows to the expected range
 
 test_stations_metadata_nrows <-
   function(df) {
@@ -32,6 +44,10 @@ test_stations_metadata_nrows <-
     }
   }
 
+#this function it's designed to check whether the data types (column types) of 
+#the columns in a given data frame df match the expected data types, in this 
+#case they should be ("character", "character", "double", "double", "double")
+
 test_stations_metadata_coltypes <-
   function(df) {
     expected_coltypes <-
@@ -44,7 +60,11 @@ test_stations_metadata_coltypes <-
       print("FAIL: Columns do not have the correct specification")
     }
   }
-  
+
+#this function it's designed to check the number of missing (NA) values in a 
+#given data frame df and determine whether the amount of missing data is within 
+#an acceptable limit
+
 test_stations_metadata_nmissing <-
   function(df) {
     max_miss_vals <- 200
@@ -56,6 +76,10 @@ test_stations_metadata_nmissing <-
     }
   }
 
+#this function it's designed to check the time zone associated with the 
+#latestData column in a given data frame df and determine whether it matches 
+#the expected time zone, which is "UTC"
+
 test_stations_metadata_latestdata_timezone <-
   function(df) {
     
@@ -66,6 +90,11 @@ test_stations_metadata_latestdata_timezone <-
     }
   }
 
+#this function it's designed to be a composite test function that calls 
+#the previous test functions to assess the quality and correctness of a 
+#data frame df. The purpose of this function is to perform a comprehensive 
+#quality check on the data frame by running a series of individual tests 
+#and reporting the results.
 
 test_stations_metadata <- 
   function(df){
