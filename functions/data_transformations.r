39b6337b1217a49cc8f6cff983494e880fb14522
angelicavertua <146018290+angelicavertua@users.noreply.github.com>
2023-10-11 17:34:31 +0200

    creating a function to generate volume queries in Vegvesen format

--- functions/data_transformations.r
+++ functions/data_transformations.r
@@ -12,10 +12,42 @@ transform_metadata_to_df <- function(stations_metadata) {
     unnest_wider(latLon)
 }
 
+#function that should return the date time variable in ISO8601 format, 
+#with the offset added. There should be a letter "Z" appended to the end 
+#of the date string, to indicate the the time zone is UTC
 
 to_iso8601 <- function(date_time_var, offset_in_days) {
   modified_date <- lubridate::as_datetime(date_time_var) + lubridate::days(offset_in_days)
   iso8601_date <- paste(format(modified_date, tz="UTC"), "Z", sep = "")
   return(iso8601_date)
 }
+
+# Create a function to generate volume queries in Vegvesen format
+
+generate_vegvesen_volume_query <- function(station_id, from_date, to_date) {
+  query <- glue::glue("
+    {{
+      trafficData(trafficRegistrationPointId: '{station_id}') {{
+        volume {{
+          byHour(from: '{from_date}', to: '{to_date}') {{
+            edges {{
+              node {{
+                from
+                to
+                total {{
+                  volumeNumbers {{
+                    volume
+                  }}
+                }}
+              }}
+            }}
+          }}
+        }}
+      }}
+    }}
+  ")
+  
+  return(query)
+}
+
 
